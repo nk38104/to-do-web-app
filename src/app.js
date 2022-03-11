2534dfa52e2bf5b5770f5c4a381767ab2a8cbbad
Nikola Kelava <nikola.kelava94@gmail.com>
2022-03-11 18:57:17 +0100

    Add Procfile and updated .gitignore

--- src/app.js
+++ src/app.js
@@ -132,6 +132,7 @@ app.post("/delete", function(req, res) {
 });
 
 
-app.listen(3000, function() {
-    console.log("Server is listening on port 3000.");
+app.listen(process.env.PORT || 3000, function() {
+    // console.log("Server is listening on port 3000.");
+    console.log("Server started successfully...");
 });
